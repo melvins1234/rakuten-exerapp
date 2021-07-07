08c3b0a5ac80a8998b830ef38653ae12cfd63fcb
Codetinerant <jesray.garciano@gmail.com>
2021-07-07 16:59:26 +0800

    Repository and Service added

--- src/main/java/com/rakuten/rakutenweb/repository/ProductRepository.java
+++ src/main/java/com/rakuten/rakutenweb/repository/ProductRepository.java
@@ -1,4 +1,8 @@
 package com.rakuten.rakutenweb.repository;
 
-public interface ProductRepository {
+import com.rakuten.rakutenweb.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
---
+++ src/main/java/com/rakuten/rakutenweb/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.rakuten.rakutenweb.service;
+
+public class ProductService {
+}
