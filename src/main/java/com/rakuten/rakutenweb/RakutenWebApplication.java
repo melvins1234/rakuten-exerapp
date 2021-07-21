3f96df7626848eb28e0352146052fa9ca131d9eb
Melvin Baybayanon <melvsbaybayanon@gmail.com>
2021-07-21 20:20:17 +0800

    fix deploy AWS

--- src/main/java/com/rakuten/rakutenweb/RakutenWebApplication.java
+++ src/main/java/com/rakuten/rakutenweb/RakutenWebApplication.java
@@ -2,14 +2,21 @@ package com.rakuten.rakutenweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.rakuten.rakutenweb"} )
-public class RakutenWebApplication {
+public class RakutenWebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RakutenWebApplication.class, args);
 	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RakutenWebApplication.class);
+    }
 
 }
