486cc35a3424647a1da0328772d2f2034127d3d8
Melvin Baybayanon <melvsbaybayanon@gmail.com>
2021-07-22 20:32:35 +0800

    Fix

--- src/main/java/com/rakuten/rakutenweb/RakutenWebApplication.java
+++ src/main/java/com/rakuten/rakutenweb/RakutenWebApplication.java
@@ -2,11 +2,12 @@ package com.rakuten.rakutenweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.rakuten.rakutenweb"} )
-public class RakutenWebApplication {
+public class RakutenWebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RakutenWebApplication.class, args);
