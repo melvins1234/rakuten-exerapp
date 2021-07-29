c1c9d93522559ece4a4a397adf3a29cf7288fa83
Melvin Baybayanon <melvsbaybayanon@gmail.com>
2021-07-29 19:02:30 +0800

    Login

--- src/main/java/com/rakuten/rakutenweb/RakutenWebApplication.java
+++ src/main/java/com/rakuten/rakutenweb/RakutenWebApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.rakuten.rakutenweb"} )
@@ -18,5 +22,22 @@ public class RakutenWebApplication extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(RakutenWebApplication.class);
     }
+    
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/resources/**","/images/**").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.formLogin()
+				.loginPage("/signin").permitAll();
+		}
+	}
 
 }
+
