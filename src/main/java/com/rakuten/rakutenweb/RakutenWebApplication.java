77e11990033ad8498e7fde4dcf4451ad0dac8a07
Melvin Baybayanon <melvsbaybayanon@gmail.com>
2021-07-29 18:20:13 +0800

    Login and Register

--- src/main/java/com/rakuten/rakutenweb/RakutenWebApplication.java
+++ src/main/java/com/rakuten/rakutenweb/RakutenWebApplication.java
@@ -2,10 +2,7 @@ package com.rakuten.rakutenweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.boot.builder.SpringApplicationBuilder;
-=======
->>>>>>> 486cc35a3424647a1da0328772d2f2034127d3d8
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
